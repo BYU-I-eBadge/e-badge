1792b5e5745123b17698871310d814b3d32e8741
JimTheScientist <jimthescientist@pounce.dev>
2025-09-25 17:06:05 -0600

    Changed screen test to use hardware SPI

--- test/Testing Code/Screen-Test/Screen-Test.ino
+++ test/Testing Code/Screen-Test/Screen-Test.ino
@@ -3,34 +3,34 @@
 
 
 /**************************************************************************
-  This is a library for several Adafruit displays based on ST77* drivers.
-
-  This example works with the 1.14" TFT breakout
-    ----> https://www.adafruit.com/product/4383
-  The 1.3" TFT breakout
-    ----> https://www.adafruit.com/product/4313
-  The 1.47" TFT breakout
-    ----> https://www.adafruit.com/product/5393
-  The 1.54" TFT breakout
-    ----> https://www.adafruit.com/product/3787
-  The 1.69" TFT breakout
-    ----> https://www.adafruit.com/product/5206
-  The 1.9" TFT breakout
-    ----> https://www.adafruit.com/product/5394
-  The 2.0" TFT breakout
-    ----> https://www.adafruit.com/product/4311
-
-
-  Check out the links above for our tutorials and wiring diagrams.
-  These displays use SPI to communicate, 4 or 5 pins are required to
-  interface (RST is optional).
-
-  Adafruit invests time and resources providing this open source code,
-  please support Adafruit and open-source hardware by purchasing
-  products from Adafruit!
-
-  Written by Limor Fried/Ladyada for Adafruit Industries.
-  MIT license, all text above must be included in any redistribution
+ * This is a library for several Adafruit displays based on ST77* drivers.
+ *
+ * This example works with the 1.14" TFT breakout
+ *   ----> https://www.adafruit.com/product/4383
+ * The 1.3" TFT breakout
+ *   ----> https://www.adafruit.com/product/4313
+ * The 1.47" TFT breakout
+ *   ----> https://www.adafruit.com/product/5393
+ * The 1.54" TFT breakout
+ *   ----> https://www.adafruit.com/product/3787
+ * The 1.69" TFT breakout
+ *   ----> https://www.adafruit.com/product/5206
+ * The 1.9" TFT breakout
+ *   ----> https://www.adafruit.com/product/5394
+ * The 2.0" TFT breakout
+ *   ----> https://www.adafruit.com/product/4311
+ *
+ *
+ * Check out the links above for our tutorials and wiring diagrams.
+ * These displays use SPI to communicate, 4 or 5 pins are required to
+ * interface (RST is optional).
+ *
+ * Adafruit invests time and resources providing this open source code,
+ * please support Adafruit and open-source hardware by purchasing
+ * products from Adafruit!
+ *
+ * Written by Limor Fried/Ladyada for Adafruit Industries.
+ * MIT license, all text above must be included in any redistribution
  **************************************************************************/
 
 #include <Adafruit_GFX.h>    // Core graphics library
@@ -44,14 +44,15 @@
 #define TFT_MOSI 35  // Data out
 #define TFT_SCLK 36  // Clock out
 
-Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_MOSI, TFT_SCLK, TFT_RST);
+SPIClass SPI_2(HSPI);  // or VSPI depending on ESP32 variant
 
+Adafruit_ST7789 tft(&SPI_2, TFT_CS, TFT_DC, TFT_RST);
 float p = 3.1415926;
 
 void setup(void) {
   Serial.begin(9600);
   Serial.print(F("Hello! ST77xx TFT Test"));
-
+  SPI_2.begin(TFT_SCLK, -1, TFT_MOSI, -1);
   // OR use this initializer (uncomment) if using a 2.0" 320x240 TFT:
   tft.init(240, 320);           // Init ST7789 320x240
 
